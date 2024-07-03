4534c5d6684f0773fbc8e7ceebbf9d4e8db4cd7d
evanderzwan <evanderzwan@gmail.com>
2024-07-03 15:16:11 +0200

    Add project files.

---
+++ Monad/ClassList.cs
@@ -0,0 +1,31 @@
+﻿namespace Monad;
+
+public sealed class ClassList
+{
+    private readonly List<string> _classNames = [];
+
+    public ClassList Add(string className, bool condition)
+    {
+        if (condition)
+        {
+            _classNames.Add(className);
+        }
+
+        return this;
+    }
+
+    public ClassList AddIfNotEmpty(string? className)
+        => Add(className!, className is { Length: > 0 });
+
+    public static ClassList Create(string className, bool condition = true)
+        => new ClassList().Add(className, condition);
+
+    public override string ToString()
+        => string.Join(' ', _classNames);
+
+    public static implicit operator string(ClassList list)
+        => list.ToString();
+
+    public static implicit operator ClassList(string str)
+        => Create(str);
+}
