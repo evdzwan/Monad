71ead98ceb1172dc92302ae1fc0b32c7a2a97618
evanderzwan <evanderzwan@gmail.com>
2024-07-04 17:44:00 +0200

    update

---
+++ Monad/GridDimension.cs
@@ -0,0 +1,21 @@
+﻿namespace Monad;
+
+public sealed class GridDimension
+{
+    private GridDimension(string value)
+        => Value = value;
+
+    internal string Value { get; }
+
+    public static GridDimension Auto
+        => new("auto");
+
+    public static GridDimension Exact(int sizeInPixels)
+        => new($"{sizeInPixels}px");
+
+    public static GridDimension Fill(int factor = 1)
+        => new($"{factor}fr");
+
+    public override string ToString()
+        => Value;
+}
