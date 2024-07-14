d2d4c3087192b5c1f5cc0ca8ad2e4d869c40fb38
evanderzwan <evanderzwan@gmail.com>
2024-07-14 19:18:03 +0200

    update

--- Monad/Components/Controls/DataGridExpressionColumn{TItem,TValue}.cs
+++ Monad/Components/Controls/DataGridExpressionColumn{TItem,TValue}.cs
@@ -8,25 +8,11 @@ public sealed class DataGridExpressionColumn<TItem, TValue> : DataGridColumn<TIt
 {
     private Func<TItem, TValue> CompiledValue { get; set; } = default!;
 
-    private RenderFragment<TItem>? DefaultEditContent { get; set; }
-
     [Parameter, EditorRequired, Description("Value expression.")]
     public required Expression<Func<TItem, TValue>> Value { get; set; }
 
-    protected override RenderFragment<(TItem Item, bool Edit)> CreateCellContent()
-    {
-        return new(row => builder =>
-        {
-            if (row.Edit)
-            {
-                (EditContent ?? DefaultEditContent)?.Invoke(row.Item)(builder);
-            }
-            else
-            {
-                builder.AddContent(sequence: 0, CompiledValue(row.Item));
-            }
-        });
-    }
+    protected override RenderFragment<TItem> CreateCellContent()
+        => item => builder => builder.AddContent(sequence: 0, CompiledValue(item));
 
     protected override RenderFragment CreateHeaderContent()
         => builder => builder.AddContent(sequence: 0, Title ?? (Value.Body is MemberExpression me ? me.Member.Name : null));
@@ -38,21 +24,6 @@ public sealed class DataGridExpressionColumn<TItem, TValue> : DataGridColumn<TIt
         if (Value != prevValue)
         {
             CompiledValue = Value.Compile();
-            DefaultEditContent = item => builder =>
-            {
-                builder.OpenComponent<Editor<TValue>>(sequence: 0);
-                builder.AddComponentParameter(sequence: 1, nameof(Editor<TValue>.Value), CompiledValue(item));
-                builder.AddComponentParameter(sequence: 2, nameof(Editor<TValue>.ValueChanged), EventCallback.Factory.Create<TValue>(this, SetValue));
-                //builder.AddComponentParameter(sequence: 3, nameof(Editor<TValue>.ValueExpression), Value);
-                builder.CloseComponent();
-            };
         }
-    }
-
-    private void SetValue(TValue value)
-    {
-        //TODO dit moet beter kunnen
-        //var prop = Value.GetPropertyInfo();
-        //prop.SetValue(Value.GetContext(), value);
     }
 }
--- Monad/Components/Controls/DataGridTemplateColumn{TItem}.cs
+++ Monad/Components/Controls/DataGridTemplateColumn{TItem}.cs
@@ -8,6 +8,6 @@ public sealed class DataGridTemplateColumn<TItem> : DataGridColumn<TItem>
     [Parameter, EditorRequired, Description("Cell content.")]
     public required RenderFragment<TItem> ChildContent { get; set; }
 
-    protected override RenderFragment<(TItem Item, bool Edit)> CreateCellContent()
-        => row => builder => (row.Edit ? EditContent : ChildContent)?.Invoke(row.Item)(builder);
+    protected override RenderFragment<TItem> CreateCellContent()
+        => item => builder => ChildContent(item)(builder);
 }
