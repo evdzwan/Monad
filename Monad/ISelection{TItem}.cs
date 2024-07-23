596184ec27eaa1603a1f4bcaa56663faefd89765
evanderzwan <evanderzwan@gmail.com>
2024-07-23 14:20:54 +0200

    update

--- Monad/ISelection{TItem}.cs
+++ Monad/ISelection{TItem}.cs
@@ -6,11 +6,15 @@ public interface ISelection<TItem>
 
     internal ICollection<TItem> Target { get; }
 
+    [Description("Adds <code>item</code> to target selection.")]
     void Activate(TItem item);
 
+    [Description("Removes <code>item</code> from target selection.")]
     void Deactivate(TItem item);
 
+    [Description("Checks wether <code>item</code> exists in target selection.")]
     bool IsActive(TItem item);
 
+    [Description("Adds <code>item</code> when not already active; removes it otherwise.")]
     void Toggle(TItem item);
 }
