2aa99f89fd9b9fb76204695228efa6271a12b610
evanderzwan <evanderzwan@gmail.com>
2024-07-23 12:05:35 +0200

    update

---
+++ Demo/Examples/Tools/ClassList/Example.cs
@@ -0,0 +1,12 @@
+﻿// create a class list by the empty ctor or like this; the second (bool) parameter is optional
+var list = ClassList.Create("my-class");
+
+// add classes like this; here the second parameter is also optional (true by default)
+list.Add("disabled", IsDisabled);
+
+// class list is commonly used like this
+public string CssClass => ClassList.Create("my-class")
+                                   .Add("active", IsActive)
+                                   .Add("disabled", IsDisabled);
+
+// the resulting class for a non-active, disabled state would be 'my-class disabled'
---
+++ Demo/Examples/Tools/StyleList/Example.cs
@@ -0,0 +1,11 @@
+﻿// create a style list by the empty ctor or like this; the third (bool) parameter is optional
+var list = StyleList.Create("background-color", "red");
+
+// add styles like this; here the third parameter is also optional (true by default)
+list.Add("opacity", "0", IsHidden);
+
+// style list is commonly used like this
+public string CssStyle => StyleList.Create("background-color", "red")
+                                   .Add("opacity", "0", IsHidden);
+
+// the resulting style for a non-hidden state would be 'background-color:red'
