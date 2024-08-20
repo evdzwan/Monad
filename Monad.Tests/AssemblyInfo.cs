52978a3c6799594e8094575f2105b52c3ac2fd07
evanderzwan <evanderzwan@gmail.com>
2024-08-20 16:39:01 +0200

    added editor control and some examples

---
+++ Monad.Tests/AssemblyInfo.cs
@@ -0,0 +1,1 @@
+﻿[assembly: FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
---
+++ Monad/EditorType.cs
@@ -0,0 +1,20 @@
+﻿namespace Monad;
+
+public enum EditorType
+{
+    Auto,
+    Check,
+    Date,
+    DateTime,
+    Email,
+    Month,
+    MultiLine,
+    Number,
+    Password,
+    Radio,
+    Range,
+    Telephone,
+    Text,
+    Time,
+    Url
+}
