eef00c4759ece962749c5ed4196580bed7d2a56b
evanderzwan <evanderzwan@gmail.com>
2024-08-21 23:02:12 +0200

    added tests

---
+++ Monad.Tests/Components/Controls/ResponderTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Monad.Components.Controls;
+
+internal sealed class ResponderTests : BUnitTestContext
+{
+    [Test]
+    public void TestDefault()
+    {
+        var responder = RenderComponent<Responder>(builder => builder.Add(c => c.Default, "fake-content"));
+        responder.MarkupMatches("""
+            <div class="responder">
+                <div class="responder-content">fake-content</div>
+                <div class="responder-content-sm"></div>
+                <div class="responder-content-md"></div>
+                <div class="responder-content-lg"></div>
+            </div>
+            """);
+    }
+
+    [Test]
+    public void TestLarge()
+    {
+        var responder = RenderComponent<Responder>(builder => builder.Add(c => c.Large, "fake-content"));
+        responder.MarkupMatches("""
+            <div class="responder">
+                <div class="responder-content"></div>
+                <div class="responder-content-sm"></div>
+                <div class="responder-content-md"></div>
+                <div class="responder-content-lg">fake-content</div>
+            </div>
+            """);
+    }
+
+    [Test]
+    public void TestMedium()
+    {
+        var responder = RenderComponent<Responder>(builder => builder.Add(c => c.Medium, "fake-content"));
+        responder.MarkupMatches("""
+            <div class="responder">
+                <div class="responder-content"></div>
+                <div class="responder-content-sm"></div>
+                <div class="responder-content-md">fake-content</div>
+                <div class="responder-content-lg"></div>
+            </div>
+            """);
+    }
+
+    [Test]
+    public void TestSmall()
+    {
+        var responder = RenderComponent<Responder>(builder => builder.Add(c => c.Small, "fake-content"));
+        responder.MarkupMatches("""
+            <div class="responder">
+                <div class="responder-content"></div>
+                <div class="responder-content-sm">fake-content</div>
+                <div class="responder-content-md"></div>
+                <div class="responder-content-lg"></div>
+            </div>
+            """);
+    }
+}
---
+++ Monad.Tests/SizeTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Monad;
+
+internal sealed class SizeTests
+{
+    [Test]
+    public void TestAuto()
+    {
+        var size = Size.Auto;
+        Assert.That(size.Value, Is.EqualTo("auto"));
+    }
+
+    [Test]
+    public void TestFraction()
+    {
+        var size = Size.Fraction(1.2);
+        Assert.That(size.Value, Is.EqualTo("1.2fr"));
+    }
+
+    [Test]
+    public void TestPercentage()
+    {
+        var size = Size.Percentage(24.8);
+        Assert.That(size.Value, Is.EqualTo("24.8%"));
+    }
+
+    [Test]
+    public void TestPixels()
+    {
+        var size = Size.Pixels(4.2);
+        Assert.That(size.Value, Is.EqualTo("4.2px"));
+    }
+
+    [Test]
+    public void TestToString()
+    {
+        var size = new Size("42em");
+        Assert.That(size.ToString(), Is.EqualTo("42em"));
+    }
+
+    [Test]
+    public void TestValue()
+    {
+        var size = new Size("42em");
+        Assert.That(size.Value, Is.EqualTo("42em"));
+    }
+}
