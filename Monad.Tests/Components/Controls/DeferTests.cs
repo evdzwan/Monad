990e7b7c2812a6552b72e90001466c6ae04cfcfd
evanderzwan <evanderzwan@gmail.com>
2024-08-21 12:08:23 +0200

    added selector and tests

---
+++ Monad.Tests/Components/Controls/DeferTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Monad.Components.Controls;
+
+internal sealed class DeferTests : BUnitTestContext
+{
+    [Test]
+    public void TestChildContent()
+    {
+        var defer = RenderComponent<Defer>(builder => builder.AddChildContent("fake-content"));
+        defer.MarkupMatches("fake-content");
+    }
+}
