dab08503549823c1e0a4bf5aa8f4db753058f7b0
Erik van der Zwan <E.van.der.Zwan@chipsoft.com>
2024-08-23 16:01:57 +0200

    added overlay control

---
+++ Monad.Tests/Components/Controls/OverlayTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Monad.Components.Controls;
+
+internal sealed class OverlayTests : BUnitTestContext
+{
+    [Test]
+    public void TestChildContent()
+    {
+        var overlay = RenderComponent<Overlay>(builder => builder.AddChildContent("fake-content"));
+        overlay.MarkupMatches("""<div class="overlay">fake-content</div>""");
+    }
+}
