669f3c6cc2dded2797f518207cfd7bf59acf6cb5
Erik van der Zwan <E.van.der.Zwan@chipsoft.com>
2024-08-23 15:01:55 +0200

    added tests for chart en gauge

--- Monad.Tests/Components/Controls/ChartTests.cs
+++ Monad.Tests/Components/Controls/ChartTests.cs
@@ -1,5 +1,46 @@
-﻿namespace Monad.Components.Controls;
+﻿using AngleSharp.Css.Dom;
+
+namespace Monad.Components.Controls;
 
 internal sealed class ChartTests : BUnitTestContext
 {
+    [Test]
+    public void TestItems()
+    {
+        var chart = RenderComponent<Chart<int>>(builder => builder.Add(c => c.Items, []));
+        Assert.That(chart.FindAll(".chart-entity"), Is.Empty);
+
+        chart.SetParametersAndRender(builder => builder.Add(c => c.Items, [1, 2, 3]));
+        Assert.That(chart.FindAll(".chart-entity"), Has.Count.EqualTo(3));
+    }
+
+    [Test]
+    public void TestItemValue()
+    {
+        var chart = RenderComponent<Chart<int>>(builder => builder.Add(c => c.Items, [1]));
+        var entity = chart.Find(".chart-entity");
+        Assert.That(entity.GetStyle().GetPropertyValue("--value"), Is.EqualTo("1"));
+
+        chart.SetParametersAndRender(builder => builder.Add(c => c.ItemValue, v => 42));
+        entity = chart.Find(".chart-entity");
+        Assert.That(entity.GetStyle().GetPropertyValue("--value"), Is.EqualTo("42"));
+    }
+
+    [Test]
+    public void TestType()
+    {
+        var chart = RenderComponent<Chart<int>>(builder => builder.Add(c => c.Items, [1, 2, 3]));
+        Assert.Multiple(() =>
+        {
+            Assert.That(chart.FindAll(".chart.area"), Is.Not.Empty);
+            Assert.That(chart.FindAll(".chart.bar"), Is.Empty);
+        });
+
+        chart.SetParametersAndRender(builder => builder.Add(c => c.Type, ChartType.Bar));
+        Assert.Multiple(() =>
+        {
+            Assert.That(chart.FindAll(".chart.area"), Is.Empty);
+            Assert.That(chart.FindAll(".chart.bar"), Is.Not.Empty);
+        });
+    }
 }
--- Monad.Tests/Components/Controls/GaugeTests.cs
+++ Monad.Tests/Components/Controls/GaugeTests.cs
@@ -1,5 +1,51 @@
-﻿namespace Monad.Components.Controls;
+﻿using AngleSharp.Css.Dom;
+
+namespace Monad.Components.Controls;
 
 internal sealed class GaugeTests : BUnitTestContext
 {
+    [Test]
+    public void TestMaximum()
+    {
+        var gauge = RenderComponent<Gauge>(builder => builder.Add(c => c.Maximum, 90));
+        var entity = gauge.Find(".gauge-entity");
+
+        Assert.That(entity.GetStyle().GetPropertyValue("--value-max"), Is.EqualTo("90"));
+    }
+
+    [Test]
+    public void TestMinimum()
+    {
+        var gauge = RenderComponent<Gauge>(builder => builder.Add(c => c.Minimum, 10));
+        var entity = gauge.Find(".gauge-entity");
+
+        Assert.That(entity.GetStyle().GetPropertyValue("--value-min"), Is.EqualTo("10"));
+    }
+
+    [Test]
+    public void TestType()
+    {
+        var gauge = RenderComponent<Gauge>();
+        Assert.Multiple(() =>
+        {
+            Assert.That(gauge.FindAll(".gauge.circle"), Is.Not.Empty);
+            Assert.That(gauge.FindAll(".gauge.ring"), Is.Empty);
+        });
+
+        gauge.SetParametersAndRender(builder => builder.Add(c => c.Type, GaugeType.Ring));
+        Assert.Multiple(() =>
+        {
+            Assert.That(gauge.FindAll(".gauge.circle"), Is.Empty);
+            Assert.That(gauge.FindAll(".gauge.ring"), Is.Not.Empty);
+        });
+    }
+
+    [Test]
+    public void TestValue()
+    {
+        var gauge = RenderComponent<Gauge>(builder => builder.Add(c => c.Value, 42));
+        var entity = gauge.Find(".gauge-entity");
+
+        Assert.That(entity.GetStyle().GetPropertyValue("--value"), Is.EqualTo("42"));
+    }
 }
