f846bcc16f409a3af4dd967160addb53b33d3036
evanderzwan <evanderzwan@gmail.com>
2024-09-06 22:54:29 +0200

    Added Size demo page

---
+++ Monad.Demo/Examples/Tools/Size.cs
@@ -0,0 +1,14 @@
+// Value = "auto"
+var autoSize = Size.Auto;
+
+// Value = "4.2fr"
+var fractionSize = Size.Fraction(4.2);
+
+// Value = "42%"
+var percentageSize = Size.Percentage(42);
+
+// Value = "4.2px"
+var pixelsSize = Size.Pixels(4.2);
+
+// Value = "4.2rem"
+var customSize = new Size("4.2rem");
--- Monad/Size.cs
+++ Monad/Size.cs
@@ -4,19 +4,25 @@ namespace Monad;
 
 public sealed class Size(string value)
 {
+    [Description("Creates a new instance with value <code>auto</code>.")]
     public static Size Auto { get; } = new("auto");
 
+    [Description("String representation of the size.")]
     public string Value { get; } = value;
 
+    [Description("Creates a new instance with value <code>{fraction}fr</code>.")]
     public static Size Fraction(double fraction)
         => new($"{fraction.ToString(CultureInfo.InvariantCulture)}fr");
 
+    [Description("Creates a new instance with value <code>{percentage}%</code>.")]
     public static Size Percentage(double percentage)
         => new($"{percentage.ToString(CultureInfo.InvariantCulture)}%");
 
+    [Description("Creates a new instance with value <code>{pixels}px</code>.")]
     public static Size Pixels(double pixels)
         => new($"{pixels.ToString(CultureInfo.InvariantCulture)}px");
 
+    [Description("Returns <code>Value</code> property.")]
     public override string ToString()
         => Value;
 }
